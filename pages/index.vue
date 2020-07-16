bcd370fee3adadcaa69609a415faa5b88c9ef5f7
Pico12 <github@pico12.fr>
2020-07-16 12:21:00 +0200

    Cleaning.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
-<div>
-    <h1>Liste des languages: </h1>
-    <h3 v-for="(language, index) in languages" :key="index" :language="language" :data-index="index" >
-    {{language}}
-    </h3>
-</div>
+    <v-container fluid>
+        <v-row justify="space-around">
+            <v-col justify="space-around">
+                <h1 class="display-1">Select language 1:</h1>
+                <v-select v-model="language1" :items="languages" item-value="id" item-text="name"></v-select>
+            </v-col>
+            <v-col justify="space-around">
+                <h1 class="display-1">Select language 2:</h1>
+                <v-select v-model="language2" :items="languages" item-value="id" item-text="name"></v-select>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
